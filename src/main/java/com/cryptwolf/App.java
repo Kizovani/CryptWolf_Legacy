24a747461fbb7fca11a821258301d3ad46e1f647
Kizovani <perailaki@gmail.com>
2024-05-28 21:33:55 -0400

    added cryptwolf_icon.jpg as default icon for the application

--- src/main/java/com/cryptwolf/App.java
+++ src/main/java/com/cryptwolf/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +20,8 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
+        Image wolfIcon = new Image(App.class.getResourceAsStream("/com/cryptwolf/icons/cryptwolf_icon.jpg"));
+        stage.getIcons().add(wolfIcon);
         stage.show();
     }
 
